103cb27b1df57478c59fd121f6a2ae7df4eabce5
Jorge <jvicenteza@alumnos.unex.es>
2021-12-22 12:11:32 +0100

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+// import { ContadorComponent } from './contador/contador/contador.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { ListadoComponent } from './heroes/listado/listado.component';
+import { ContadorModule } from './contador/contador/contador.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // ContadorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
